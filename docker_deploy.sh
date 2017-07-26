772bf015d3338f71475fa72c4346eac871371ba1
Michael Herman <hermanmu@gmail.com>
2017-07-25 18:07:10 -0600

    updated travis for ecr (part 5)

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_BRANCH" == "development" ]; then
     docker login -e $DOCKER_EMAIL -u $DOCKER_ID -p $DOCKER_PASSWORD
-    export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+    export TAG="latest"
     # users
     docker build $USERS_REPO -t $USERS:$COMMIT
     docker tag $USERS:$COMMIT $DOCKER_ID/$USERS:$TAG
@@ -23,4 +23,30 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     docker build $NGINX_REPO -t $NGINX:$COMMIT
     docker tag $NGINX:$COMMIT $DOCKER_ID/$NGINX:$TAG
     docker push $DOCKER_ID/$NGINX
+fi
+
+if [ "$TRAVIS_BRANCH" == "staging" ]; then
+    # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
+    eval $(aws ecr get-login --region us-east-1)
+    export TAG="latest"
+    # users
+    docker build $USERS_REPO -t $USERS:$COMMIT
+    docker tag $USERS:$COMMIT $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$USERS:$TAG
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$USERS:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT
+    docker tag $USERS_DB:$COMMIT $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$USERS_DB:$TAG
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$USERS_DB:$TAG
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT
+    docker tag $CLIENT:$COMMIT $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$CLIENT:$TAG
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$CLIENT:$TAG
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT
+    docker tag $SWAGGER:$COMMIT $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$SWAGGER:$TAG
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$SWAGGER:$TAG
+    # nginx
+    docker build $NGINX_REPO -t $NGINX:$COMMIT
+    docker tag $NGINX:$COMMIT $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$NGINX:$TAG
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/$NGINX:$TAG
 fi
