fc7a194f9435582ec590f1c5608ead305d73ea39
Michael Herman <hermanmu@gmail.com>
2017-07-31 21:56:38 -0600

    updated travis for ecs (part 5)

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -6,6 +6,8 @@ then
   if [ "$TRAVIS_BRANCH" == "staging" ]
   then
 
+    JQ="jq --raw-output --exit-status"
+
     configure_aws_cli() {
     	aws --version
     	aws configure set default.region us-east-1
@@ -20,11 +22,11 @@ then
     }
 
     register_definition() {
-      if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" --family $family); then
-          echo "Revision: $revision"
+      if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
       else
-          echo "Failed to register task definition"
-          return 1
+        echo "Failed to register task definition"
+        return 1
       fi
     }
 
@@ -37,9 +39,7 @@ then
       make_task_def
       register_definition
 
-      echo $(aws ecs update-service --cluster flask-microservices-staging --service flask-microservices-staging --task-definition $revision)
-
-      if [[ $(aws ecs update-service --cluster flask-microservices-staging --service flask-microservices-staging --task-definition $revision) != $revision ]]; then
+      if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
       fi
