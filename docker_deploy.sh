396c0ccaffa0709cfd4fa98af6f3dfac96eb636a
Michael Herman <hermanmu@gmail.com>
2017-08-12 21:28:04 -0600

    ecs update

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -30,7 +30,7 @@ then
       fi
     }
 
-    update service() {
+    update_service() {
       if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
