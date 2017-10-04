b991ecaac8b1d0ccc62a431bfdb8239dfe0f88cc
Michael Herman <hermanmu@gmail.com>
2017-10-04 07:44:40 -0600

    updated e2e tests and docker compose (part 6)

--- e2e/all-users.test.js
+++ e2e/all-users.test.js
@@ -2,11 +2,11 @@ import { Selector } from 'testcafe';
 
 const TEST_URL = process.env.TEST_URL;
 
-fixture('/').page(`${TEST_URL}/`);
+fixture('/all-users').page(`${TEST_URL}/all-users`);
 
 test(`should display the page correctly if a user is not logged in`, async (t) => {
   await t
-    .navigateTo(TEST_URL)
+    .navigateTo(`${TEST_URL}/all-users`)
     .expect(Selector('H1').withText('All Users').exists).ok()
     .expect(Selector('a').withText('User Status').exists).notOk()
     .expect(Selector('a').withText('Log Out').exists).notOk()
